d91ecc692bf71967ca1f7fc453c0badd15e5b688
RyanCocking <40760012+RyanCocking@users.noreply.github.com>
2025-02-12 16:07:59 +0000

    Vite config file with relative paths

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+base: './',              // ensure relative paths, e.g. in index.html
+build: {
+    outDir: 'dist',      // gets deployed to github pages
+    assetsDir: 'assets', // static assets (images, js, css, etc.)
+},
+});
+
